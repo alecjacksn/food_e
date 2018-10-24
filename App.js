bd6373f37cab7df85909c208632bdc8c95b94cf9
alecjacksn <alecajackson@gmail.com>
2018-10-23 21:00:21 -1000

    basic nav with 3 components

--- App.js
+++ App.js
@@ -1,47 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import SettingsPage from './components/SettingsPage'
+import HomeScreen from './components/HomeScreen'
+import GoogleMaps from './components/GoogleMaps/GoogleMaps'
+import { createStackNavigator } from 'react-navigation';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const RootStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Settings: {
+    screen: SettingsPage
+  },
+  Maps: {
+    screen: GoogleMaps
+  }
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+
+
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>test!</Text>
-      </View>
+    return (      
+        <RootStack />      
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
